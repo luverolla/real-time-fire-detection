417e8db9c1883bbc8753cecb9ce49e3ea6607094
Luigi Verolla <luverolla@gmx.it>
2023-05-13 20:39:04 +0200

    replaced python classes with notebooks

--- classifiers/MEClassifier.py
+++
@@ -1,6 +0,0 @@
-import torch.nn as nn
-import ColorClassifier, MovementClassifier, ShapeClassifier
-
-
-class MEClassifier(nn.Module):
-    pass
--- classifiers/MovementClassifier.py
+++
@@ -1,5 +0,0 @@
-import torch.nn as nn
-
-
-class MovementClassifier(nn.Module):
-    pass
